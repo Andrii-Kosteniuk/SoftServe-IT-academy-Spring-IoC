9237fb2b39b8a9744f9d6c7677ccd8ca8e37a386
Augustin Gerasym <augustingerasym@gmail.com>
2024-11-11 16:29:22 +0100

    Created validation.user package with users data validators.

---
+++ src/main/java/com/softserve/itacademy/validation/UserValidator.java
@@ -0,0 +1,33 @@
+package com.softserve.itacademy.validation;
+
+public class UserValidator {
+
+    public static boolean isValidFirstName(String firstName) {
+        return validateName(firstName);
+    }
+
+    public static boolean isValidLastName(String lastName) {
+        return validateName(lastName);
+    }
+
+    public static boolean isValidEmail(String email) {
+        return validateEmail(email);
+    }
+
+    public static boolean isValidPassword(String password) {
+        return validatePassword(password);
+    }
+
+    private static boolean validateName(String name) {
+        return name.matches("[a-zA-Z]{2,}");
+    }
+
+    private static boolean validateEmail(String email) {
+        String regex = "^[a-zA-Z]+@[a-zA-Z]+\\.[a-zA-Z]+$";
+        return email.matches(regex);
+    }
+
+    private static boolean validatePassword(String password) {
+        return password.matches("[a-zA-Z0-9]{8,}");
+    }
+}
